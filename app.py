817cc4f893013eb1511ae05221d9e2a3ab3e2d29
Angelo Basile <92684587+carni123@users.noreply.github.com>
2025-01-10 12:39:13 +0000

    sta un casino

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-app.run()
+@app.route("/")
+def home():
+    return render_template("home.html",titolo="Register")
+
+@app.route("/register")
+def register():
+    return render_template("register.html",titolo="Register")
+
+@app.route("/log_in")
+def log_in():
+    return render_template("log_in.html",titolo="Log in")
+
+@app.route("/personale")
+def log_out():
+    return render_template("personale.html",titolo="Personale")
+
+
+app.run(debug=True)
